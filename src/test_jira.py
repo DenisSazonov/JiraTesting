f899bb27c8eaa73211c1592648fa92a2d3f47dcc
Denis Sazonov <denis_sazonov@epam.com>
2019-12-06 15:43:22 +0300

    initial commit

---
+++ src/test_jira.py
@@ -0,0 +1,22 @@
+import requests
+
+BASE_URL = 'https://jira.hillel.it'
+
+
+class WorkingWithIssues:
+    def get_issue(self, issue_id):
+        pass
+
+    def add_issue(self, issue_id):
+        pass
+
+    def edit_issue(self, issue_id):
+        pass
+
+    def delete_issue(self, issue_id):
+        pass
+
+
+class Authentification:
+    def authentification(self):
+        pass
